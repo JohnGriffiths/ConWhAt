cdc3569a9e27d72f0fc8c9db2809e574c2d3f118
John Griffiths <j.davidgriffiths@gmail.com>
2017-04-21 15:56:39 -0400

    more quick tests

--- conwhat/scratch/test_volatlas_functions.py
+++ conwhat/scratch/test_volatlas_functions.py
@@ -1,24 +1,46 @@
 # Test make_group_visitation_map() function:
 
-fstr = '/mnt/hpcvl-u1/ConWhAt/data/results/visitation_maps/dipy_dsi_sd4_l2k8_sc33/%s/vismap_%s_%s_std.nii.gz'
+from conwhat.construct import make_group_visitation_map
 
-cnxn_name = '0-10'
+
+sub_vismap_base_dir = '/u1/work/hpc3230/ConWhAt/data/results/visitation_maps/dipy_dsi_sd4_l2k8_sc33'
+sub_vismap_fstr = sub_vismap_base_dir + '/{subject_id}/vismap_{subject_id}_{cnxn_name}_std.nii.gz'
+
+cnxns_list = '/u1/work/hpc3230/ConWhAt/data/results/visitation_maps/dipy_dsi_sd4_l2k8_sc33/group/vismap_grp_cnxns_list.txt'
+cnxn_names = [r[:-1] for r in open(cnxns_list,'r').readlines()]
+
+allhcpsubs = [r[:-1] for r in open('/u1/work/hpc3230/ConWhAt/code/HCP_900.txt', 'r').readlines()]
+#subs = allhcpsubs[:700]
+subs=allhcpsubs[:40]
+
+cnxn_name = cnxn_names[10]
+sub_vismaps = [sub_vismap_fstr.format(subject_id=sub,cnxn_name=cnxn_name) for sub in subs]
 grp_vismap = '/tmp/test_grp_vismap_%s.nii.gz' %cnxn_name
-subs_list = [105620,105923,106016,106319]
-sub_vismaps = [ fstr%(sub,sub,cnxn_name) for sub in subs_list]
+sub_vismaps = [sub_vismap_fstr.format(subject_id=sub,cnxn_name=cnxn_name) for sub in subs]
+
+make_group_visitation_map(cnxn_name,sub_vismaps,grp_vismap,subs_list=subs)
 
 
-make_group_visitation_map(cnxn_name,sub_vismaps,grp_vismap,subs_list=subs_list)
+
+
+
+
+
+
+
+
 
 
 # Test make_sub_visitation_maps() function:
 
+from conwhat.construct import make_sub_visitation_maps
+
 sub = 105620
-#dpy = '/mnt/jzhpcvl-u1/hcp_wuminn_l2k8_tracks_conmats/data/results/hcp_wuminn/tractography/dipy/l2k8/105620/dsi_sd4/tracks.dpy'
-dpy = '/tmp/tracks.dpy'
-parc = '/mnt/mkhpcvl-u1/hcp_wuminn_l2k8_parcs/data/results/l2k8_rois/105620/ROI_scale33_dwispace.nii.gz'
-warp = '/mnt/mkhpcvl-u1/hcp_wuminn_JHU_wmms/data/results/105620/nat2std_warp.nii.gz'
-ref = '/usr/share/fsl/data/standard/FMRIB58_FA_1mm.nii.gz'
+dpy = '/u1/hpc3520/hcp_wuminn_l2k8_tracks_conmats/data/results/hcp_wuminn/tractography/dipy/l2k8/105620/dsi_sd4/tracks.dpy'
+#dpy = '/tmp/tracks.dpy'
+parc = '/u1/hpc3399/hcp_wuminn_l2k8_parcs/data/results/l2k8_rois/105620/ROI_scale33_dwispace.nii.gz'
+warp = '/u1/hpc3399/hcp_wuminn_JHU_wmms/data/results/105620/nat2std_warp.nii.gz'
+ref = '/opt/fsl/5.0.9/fsl/data/standard/FMRIB58_FA_1mm.nii.gz'
 outdir = '/tmp/conwhat_msvm_test'
 
 
