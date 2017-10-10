24d15c784e1917c8576c4e9fe439aa6f37a8ead5
Jopn[C[C[C[Chn Griffiths <j.davidgriffiths@gmail.com>
2017-10-10 17:53:06 -0400

    initial test

---
+++ conwhat/tests/test.py
@@ -0,0 +1,16 @@
+
+
+# Test folders are in right place; including github sub repo
+
+# Test damaging a nifti file
+
+# Test damaging a streamlines file
+
+# Test lots of connectivity stuff
+
+
+ 
+
+
+
+
