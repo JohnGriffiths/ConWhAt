335171f097fad4327d5290d077b6cb738bd4fb22
John Griffiths <j.davidgriffiths@gmail.com>
2017-10-24 13:29:16 -0400

    updated atlas fetcher and nitrc urls

--- conwhat/utils/fetchers.py
+++ conwhat/utils/fetchers.py
@@ -5,8 +5,8 @@ Downloading NeuroImaging datasets: atlas datasets
 import os
 from nilearn.datasets.utils import _fetch_files
 
-def fetch_conwhat_atlas(data_dir=None, url=None, resume=True, verbose=1,
-                        uncompress=True,dataset_name=None):
+def fetch_conwhat_atlas(atlas_name,dataset_dir,resume=True, verbose=1,
+                        uncompress=True,remove_existing=False):
     """Downloads and unpacks ConWhAt atlases
 
     Parameters
@@ -14,14 +14,19 @@ def fetch_conwhat_atlas(data_dir=None, url=None, resume=True, verbose=1,
     data_dir: string
         directory where data should be downloaded and unpacked.
 
-    url: string
-        url of file to download.
+    atlas_name: string
+        name of atlas to download
 
     resume: bool
         whether to resumed download of a partly-downloaded file.
 
     verbose: int
         verbosity level (0 means no message).
+
+    remove_existing: bool
+        remove any existing folders in the target location before 
+        downloading
+
 
     Returns
     -------
@@ -37,44 +42,54 @@ def fetch_conwhat_atlas(data_dir=None, url=None, resume=True, verbose=1,
 
     """
 
-    if url is None:
 
-      if dataset_name == 'CWL2k8Sc33Vol3d100s':
-
-        url = "https://www.nitrc.org/frs/download.php/10254/" \
-              "CWL2k8Sc33Vol3d100s.zip"
-
-
-    #opts = {'uncompress': uncompress}
-    #files = [(dataset_name,url,opts)]
-    #files_ = _fetch_files(data_dir,files,resume=resume,verbose=verbose)
-
-    dataset_dir = '%s/%s' %(data_dir,dataset_name)
-
-    
     if not os.path.isdir(dataset_dir):
+      print 'dataset dir not present. creating'
+      os.makedirs(dataset_dir)
 
-      cwd = os.getcwd()
-      os.chdir(data_dir) 
-      data_file = url.split('/')[-1]
+    if atlas_name == 'CWL2k8Sc33Vol3d100s_v01':
+        url = "https://www.nitrc.org/frs/download.php/10381/CWL2k8Sc33Vol3d100s_v01.zip"
+    elif atlas_name == 'CWL2k8Sc60Vol3d100s_v01':
+        url = "https://www.nitrc.org/frs/download.php/10382/CWL2k8Sc60Vol3d100s_v01.zip"
+    else: 
+        raise NameError('requested atlas not recognized')
+
+    atlas_dir = '%s/%s' %(dataset_dir,atlas_name)
+
+    if os.path.isdir(atlas_dir):
+
+      if remove_existing == True:
+ 
+        print 'removing existing folder'
+        os.system('rm -r %s' %atlas_dir)
+
+      else: 
+
+        raise NameError('output folder already present. aborting.')
+     
+    cwd = os.getcwd()
+    os.chdir(dataset_dir) 
+    data_file = url.split('/')[-1]
       
-      if os.path.isfile(data_file):
+    if os.path.isfile(data_file):
+      if remove_existing==True:
         print '\n\nduplicate file detected - removing...'
         cmd = 'rm %s' %data_file
         print '%s' %cmd
         os.system(cmd)
 
-      print '\n\ndownloading data_file %s...' %data_file
-      cmd = 'wget %s' %url
-      print '%s' %cmd
-      os.system(cmd)
+    print '\n\ndownloading data_file %s...' %data_file
+    cmd = 'wget %s' %url
+    print '%s' %cmd
+    os.system(cmd)
  
-      print '\n\nunzipping file...'
-      cmd = 'unzip %s' %data_file
-      print '%s' %cmd
-      os.system(cmd)
+    print '\n\nunzipping file...'
+    cmd = 'unzip %s' %data_file
+    print '%s' %cmd
+    os.system(cmd)
+    print '\n\nfinished unzipping.'
 
-      os.chdir(cwd)
+    os.chdir(cwd)
 
     return dataset_dir
 
