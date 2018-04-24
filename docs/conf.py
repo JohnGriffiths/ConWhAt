666f6ece06b3f6e3c37ee1d08aef32d23bd8dd8a
John Griffiths <JohnGriffiths@users.noreply.github.com>
2018-04-24 08:53:36 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,8 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'nbsphinx',  # JG_ADD
+    'nbsphinx',  
+    'jupyter_alabaster_theme', 
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
@@ -89,7 +90,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'jupyter_alabaster_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
