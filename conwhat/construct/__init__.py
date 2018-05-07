4576df0ee54db751b07bbd582efd6652b42f0208
John Griffiths <j.davidgriffiths@gmail.com>
2018-05-07 19:49:53 -0400

    add init for construct functions

---
+++ conwhat/construct/__init__.py
@@ -0,0 +1,11 @@
+
+from volatlas_functions import (make_sub_cnxn_mappings,
+                                make_group_cnxn_visitation_map,
+                                make_sub_cnxn_visitation_map,
+                                aggregate_grp_vismap)
+
+from volatlas_workflows import create_volatlas_workflow
+
+
+
+
